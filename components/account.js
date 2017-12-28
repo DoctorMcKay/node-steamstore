69841748726ffe5aeeb1ea680040f1b654ed1c9e
Alexander Corn <mckay@doctormckay.com>
2017-12-27 23:41:56 -0500

    Make phone matching regex a bit more strict

--- components/account.js
+++ components/account.js
@@ -213,7 +213,7 @@ SteamStore.prototype.hasPhone = function(callback) {
 		var $phone = $('.phone_header_description .account_data_field');
 		var match;
 
-		if($phone && (match = $phone.text().match(/([0-9]{2})/))) {
+		if($phone && (match = $phone.text().trim().match(/([0-9]{2})($|\s)/))) {
 			// Has phone number
 			callback(null, true, match[1]);
 			return;
