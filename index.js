7be8b35ff0f55658b48994506461c6a22d24d252
Alex Corn <mckay@doctormckay.com>
2019-01-21 18:17:36 -0500

    Updated Chrome user-agent

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Request = require('request');
 const SteamID = require('steamid');
 
-const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36";
+const USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36";
 
 module.exports = SteamStore;
 
